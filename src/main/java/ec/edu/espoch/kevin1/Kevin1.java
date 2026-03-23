c7b54cc87f90fa8bea136ccd88d75acc81caa79a
avillopes72 <avilljose72@gmail.com>
2026-03-23 17:30:02 -0500

    sa

--- src/main/java/ec/edu/espoch/kevin1/Kevin1.java
+++ src/main/java/ec/edu/espoch/kevin1/Kevin1.java
@@ -1,13 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package ec.edu.espoch.kevin1;
-
-/**
- *
- * @author SO_LAB1_PC04
- */
 public class Kevin1 {
 
     public static void main(String[] args) {
