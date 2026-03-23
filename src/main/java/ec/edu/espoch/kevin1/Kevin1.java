0e8c796ba204dedab3891d0f6b961cfe5de6473a
avillopes72 <avilljose72@gmail.com>
2026-03-23 18:46:06 -0500

    dfasd

--- src/main/java/ec/edu/espoch/kevin1/Kevin1.java
+++ src/main/java/ec/edu/espoch/kevin1/Kevin1.java
@@ -1,7 +1,51 @@
 package ec.edu.espoch.kevin1;
 public class Kevin1 {
 
+    public class student{
+       public String nombreEst;
+       public boolean estadotarj;
+       public boolean ingresar(boolean estadoTarj){
+           return estadoTarj==true;
+       }
+       public boolean pedirLibro(boolean estadoLibro){
+           return estadoLibro==true;
+       }
+       public boolean conducir(boolean estadoCar){
+           return estadoCar==true;
+       }
+       public boolean enviarMendaje(boolean estadoCell){
+           return estadoCell==true;
+       }
+       public boolean enviarTarea(boolean estadoLaptop){
+           return estadoLaptop==true;
+       }
+    }
+    public class book{
+        public String nombreLibro;
+        public boolean estadoLibro;
+        public String prestar(){
+            return nombreLibro.equalsIgnoreCase("luna de ora");
+       }
+    }
+    public class car{
+        public boolean estadoCar;
+        public int combustible;
+        public float velocidad;
+        
+    }
+    public class phone{
+        public boolean estadoCell;
+        public boolean red;
+        public int bateriaCell;
+    }
+    public class laptop{
+        public boolean estadoLaptop;
+        public float almacen;
+        public boolean red;
+        public int baterialap;
+    }
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        
     }
 }
